e5bd1e73eeb8bfd4129fb7005ff313c750c8fd40
Erwin Majewski <erwin.majewski@gmail.com>
2018-05-01 22:41:46 +0200

    Add some REST API features: insert User, update User, list all Users

--- src/main/java/com/historygo/database/UserRepository.java
+++ src/main/java/com/historygo/database/UserRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String>, QueryDslPredicateExecutor<User> {
+
+    public User findById(String id);
+
+    List<User> findByPointsGreaterThan(int points);
+
+    User findByName(String name);
 }
