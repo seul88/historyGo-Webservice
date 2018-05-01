32e5f450fb7d69aca66d1b3e6b6fd70836d41c1d
Erwin Majewski <erwin.majewski@gmail.com>
2018-05-01 22:09:34 +0200

    Create users test set.

--- src/main/java/com/historygo/database/DbExecutor.java
+++ src/main/java/com/historygo/database/DbExecutor.java
@@ -21,35 +21,121 @@ public class DbExecutor implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        User erwin = new User(
-                "Erwin",
+
+
+       Place zamek = new Place(
+                "Zamek Cesarski",
+                "Zamek Cesarski w Poznaniu (niem. Königliches Residenzschloß) – najważniejsza budowla Dzielnicy Cesarskiej, powstała dla ostatniego cesarza niemieckiego i króla Prus Wilhelma II. Zamek został zaprojektowany przez Franza Schwechtena, jednak wiele elementów wprowadzono na życzenie Wilhelma II. Cesarz osobiście ustalił szczegółowy plan swojej nowej rezydencji.\";\n",
+                15
+        );
+
+       Place ratusz =  new Place(
+               "Ratusz",
+               "Ratusz w Poznaniu – renesansowy budynek stojący na poznańskim Starym Rynku, pełniący niegdyś funkcję ratusza. Z wieży ratuszowej codziennie odgrywany jest hejnał Poznania.\n" ,
+               20
+       );
+
+       Place malta = new Place(
+               "Malta",
+               "Malta Ski – ośrodek sportowo-rekreacyjny w Poznaniu, na południowym brzegu Jeziora Maltańskiego.",
+               35
+       );
+
+       Place zoo = new Place(
+               "Nowe Zoo",
+               "Nowe Zoo w Poznaniu – drugi pod względem powierzchni ogród zoologiczny w Polsce, założony w 1974 roku na Białej Górze, wschodnim klinie zieleni Poznania. Zajmuje obszar 120,68 ha usytuowanych na pagórkowatym terenie leśnym z sześcioma rozległymi stawami o łącznej powierzchni ponad 13 ha stanowiąc naturalne siedlisko fauny krajowej. Nowe Zoo słynie z kolekcji ptaków drapieżnych oraz sów, która należy do jednej z najbogatszych w Europie.",
+               30
+       );
+
+       Place politechnika = new Place(
+               "Politechnika",
+               "Politechnika Poznańska – państwowa uczelnia o profilu technicznym w Poznaniu. Największy kampus uczelni znajduje się po obu stronach ulicy Piotrowo, na terenie osiedla samorządowego Rataje. Siedziba władz uczelni (rektorat) mieści się w zabytkowym budynku na Wildzie. Za datę powstania tej uczelni przyjmuje się rok 1919, jakkolwiek status wyższej szkoły technicznej o charakterze politechniki nadała jej Rada Ministrów dopiero w 1955.",
+               25
+       );
+
+        User u1 = new User(
+                "Seul88",
                 0,
                 new ArrayList<>()
         );
 
-        User pavlo = new User(
+        User u2 = new User(
                 "Pavlo",
                 15,
                 Arrays.asList(
-                        new Place(
-                                "Ratusz",
-                                "Dawna siedziba wladz miasta",
-                                15
-                        )
+                       ratusz
                 )
         );
 
-        User bolivar = new User(
+        User u3 = new User(
                 "Bolivar",
-                11,
-                new ArrayList<Place>()
+                0,
+                Arrays.asList(
+                        ratusz, politechnika, zoo, zamek, malta
+                )
         );
 
-      //  this.userRepository.deleteAll();;
+        User u4 = new User(
+                "Matiiii",
+                0,
+                Arrays.asList(
+                         politechnika, zoo,  malta
+                )
+        );
 
-        List<User> users = Arrays.asList(erwin, pavlo, bolivar);
+        User u5 = new User(
+                "Yankos",
+                0,
+                Arrays.asList(
+                        politechnika, zoo, zamek
+                )
+        );
 
-     //   this.userRepository.saveAll(users);
+        User u6 = new User(
+                "P0LA2007",
+                0,
+                Arrays.asList(
+                        zoo, zamek, malta
+                )
+        );
+
+        User u7 = new User(
+                "RegnaR",
+                0,
+                Arrays.asList(
+                         zamek
+                )
+        );
+
+        User u8 = new User(
+                "Vlad12",
+                0,
+                Arrays.asList(
+                        zoo, malta
+                )
+        );
+
+        User u9 = new User(
+                "Yaglack",
+                0,
+                Arrays.asList(
+                        ratusz, politechnika, zoo, malta
+                )
+        );
+
+        User u10 = new User(
+                "Martin9",
+                0,
+                Arrays.asList(
+                        ratusz, politechnika, zoo, zamek, malta
+                )
+        );
+
+        this.userRepository.deleteAll();;
+
+        List<User> users = Arrays.asList(u1, u2, u3, u4, u5, u6, u7, u8, u9, u10);
+
+        this.userRepository.save(users);
 
     }
 }
--- src/main/java/com/historygo/database/UserRepository.java
+++ src/main/java/com/historygo/database/UserRepository.java
@@ -2,8 +2,9 @@ package com.historygo.database;
 
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String>, QueryDslPredicateExecutor<User> {
 }
